a2fcbc815ab0dc08bee5f1beba2a1876466003eb
HocLT <hdquang6@gmail.com>
2023-01-26 22:00:41 +0700

    spawn gem

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,6 +8,9 @@ public class Board : MonoBehaviour
     public int height = 7;
 
     public GameObject bgTilePrefab;
+
+    public Gem[] gems;
+
     // Start is called before the first frame update
 
     void Start()
@@ -25,7 +28,17 @@ public class Board : MonoBehaviour
                 GameObject bgTile = Instantiate(bgTilePrefab, pos, Quaternion.identity);
                 bgTile.transform.parent = transform;
                 bgTile.name = $"BG Tile {x}, {y}";
+
+                int gemToUse = Random.Range(0, gems.Length);
+                SpawnGem(new Vector2Int(x, y), gems[gemToUse]);
             }
         }
+    }
+
+    void SpawnGem(Vector2Int pos, Gem gemToSpawn)
+    {
+        Gem gem = Instantiate(gemToSpawn, new Vector3(pos.x, pos.y, 0f), Quaternion.identity);
+        gem.transform.parent = transform;
+        gem.name = $"Gem - {pos.x}, {pos.y}";
     }
 }
